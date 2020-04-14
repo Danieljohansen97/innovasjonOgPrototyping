696aa061c6aba55c867e3cd710ec0260b47750e0
Danieljohansen97 <danieljohansen1997@hotmail.com>
2020-04-14 07:13:33 +0200

    Add firebase

---
+++ prototype/src/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD8aGd11nnHS-_h8UXvECqknZf0OyAac4s",
+    authDomain: "hyttebytte-5f434.firebaseapp.com",
+    databaseURL: "https://hyttebytte-5f434.firebaseio.com",
+    projectId: "hyttebytte-5f434",
+    storageBucket: "hyttebytte-5f434.appspot.com",
+    messagingSenderId: "58723817341",
+    appId: "1:58723817341:web:063f777b14b8e3695d1e14"
+};
